bb688ae429c56c848678189efcf04c4482d80ff6
amutake <amutake.s@gmail.com>
2016-02-02 18:06:58 +0900

    tmp

--- theories/freer.v
+++ theories/freer.v
@@ -2,24 +2,35 @@ Set Implicit Arguments.
 Unset Strict Implicits.
 
 Require Import Ssreflect.ssreflect.
+Require Import MathClasses.interfaces.monads MathClasses.interfaces.canonical_names.
 Require Import Coq.Logic.FunctionalExtensionality.
-Require Import monad union.
+
+Require Import union.
 
 Inductive Freer (F : Type -> Type) (A : Type) : Type :=
 | Pure : A -> Freer F A
 | Impure : forall (X : Type), F X -> (X -> Freer F A) -> Freer F A.
 
-Definition freer_pure {F} {A} (a : A) := Pure F a.
-Fixpoint freer_bind {F} {A B} (ffa : Freer F A) (f : A -> Freer F B) :=
-  match ffa with
-  | Pure a => f a
-  | Impure _ fx k => Impure fx (fun x => freer_bind (k x) f)
-  end.
+Instance freer_return {F} : MonadReturn (Freer F) := fun a => @Pure F a.
+Instance freer_bind {F} : MonadBind (Freer F) :=
+  fix freer_bind {A B} f ffa :=
+    match ffa with
+    | Pure a => f a
+    | Impure _ fx k => Impure fx (fun x => freer_bind f (k x))
+    end.
 
-Program Instance freer_monad (F : Type -> Type) : Monad (Freer F) := {
-  pure := (@freer_pure F);
-  bind := (@freer_bind F)
-}.
+Program Instance freer_equiv {F} : forall A, Equiv A -> Equiv (Freer F A).
+Next Obligation.
+  apply: eq.
+Qed.
+
+Program Instance freer_monad (F : Type -> Type) : Monad (Freer F).
+Next Obligation.
+  rewrite/Proper/respectful.
+  move=> x y equiv.
+  setoid_rewrite equiv.
+
+
 Next Obligation.
   elim: ma=> //=.
   move=> X f k IH; congr (_ _).
